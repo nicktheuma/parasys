2d4087d6e6ba35ca6f8d586df951dd8e78878d4e
nicktheuma <nicktheuma@gmail.com>
2026-02-10 16:50:33 +0100

    Adding multiple material selection.

--- parasys-mvp/src/Experience.jsx
+++ parasys-mvp/src/Experience.jsx
@@ -13,19 +13,19 @@ export function Experience() {
   const leftGroupRef = useRef()
   const rightGroupRef = useRef()
 
+  const mat_Mirror = new THREE.MeshStandardMaterial( {map: null, color: '#ffffff', roughness: 0, metalness: 1})
+  const mat_Solid = new THREE.MeshStandardMaterial( {map: null, color: '#646a39', roughness: 0.8, metalness: 0.1})
+
   const { width, material, showProps } = useControls({
-    width: { value: 1.8, min: startDims.x, max: 1, step: 0.01},
-    material: { options: { Default: '#999999', Wood: '#5d4037', Modern: '#222222', Alabaster: '#f5f5f5' } },
-    showProps: true // Toggle for the books/goblets
+    width: { value: startDims.x, min: startDims.x, max: 0.3, step: 0.01},
+    material: { options: { Default: materials['chair'], Mirror: mat_Mirror, Solid: mat_Solid } },
+    showProps: true // Toggle for props
   })
 
   useFrame(() => {
     // 1. Scale the main console body
     const width_factor = width / startDims.x;
     consoleRef.current.scale.x = THREE.MathUtils.lerp(consoleRef.current.scale.x, width_factor, 0.1)
-
-    // console.log(leftGroupRef.current.position);
-    // print("test"); //INTERESTING: PRINTS SCREENSHOT OF PAGE
     
     // 2. Move the accessories on the left and right sides
     const sideOffset = (width-startDims.x)/2
@@ -36,20 +36,16 @@ export function Experience() {
   return (
     <group dispose={null}>
       {/* THE MAIN PIECE */}
-      {/* <mesh ref={consoleRef} geometry={nodes.CONSOLE001.geometry} material={materials['WOOD MATERIAL']} envMapIntensity={2.5} roughness={0.4} metalness={0.1} color={'#ffffff'} /> */}
-      {/* <mesh ref={consoleRef} geometry={nodes.C_Medieval_chair}>
-        <meshStandardMaterial color={material} roughness={0.4} />
-      </mesh> */}
-      <mesh ref={consoleRef} geometry={nodes.C_Medieval_chair.geometry} material={materials['chair']} />
+      <mesh ref={consoleRef} geometry={nodes.C_Medieval_chair.geometry} material={material}/>
       
       {/* LEFT SIDE ACCESSORIES (Stay on the left edge) */}
       <group ref={leftGroupRef} visible={showProps}>
-        <mesh geometry={nodes.L_Medieval_chair.geometry} material={materials['chair']}  />
+        <mesh geometry={nodes.L_Medieval_chair.geometry} material={material}  />
       </group>
 
       {/* RIGHT SIDE ACCESSORIES (Stay on the right edge) */}
       <group ref={rightGroupRef} visible={showProps}>
-        <mesh geometry={nodes.R_Medieval_chair.geometry} material={materials['chair']}  />
+        <mesh geometry={nodes.R_Medieval_chair.geometry} material={material}  />
       </group>
 
       {/* CENTER PIECES (Stay in the middle) */}
@@ -60,3 +56,8 @@ export function Experience() {
     </group>
   )
 }
+
+//NOTES
+
+    // console.log(leftGroupRef.current.position);
+    // print("test"); //INTERESTING: PRINTS SCREENSHOT OF PAGE
